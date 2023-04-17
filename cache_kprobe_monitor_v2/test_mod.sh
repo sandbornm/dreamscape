d1fd173ff79ea2067c42abb0b47382f36c321bdf
Ubuntu <ubuntu@ubuntu.attlocal.net>
2023-04-17 06:32:56 +0000

    +1B mem access pgm

--- cache_kprobe_monitor_v2/test_mod.sh
+++ cache_kprobe_monitor_v2/test_mod.sh
@@ -3,7 +3,8 @@
 # Run stress on CPU for 20s and obtain the PID of the process
 #stress --cpu 1 --timeout 20s &
 
-/home/ubuntu/dreamscape/test_bins/bins/arm64/cache_intensive &
+/home/ubuntu/dreamscape/test_bins/bins/arm64/onebil_memacc &
+#cache_intensive &
 pgm_pid=$!
 
 # Monitor the stress process using the kernel module
